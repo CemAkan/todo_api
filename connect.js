87e2a33a01237fa0d947521f36096970d3599e98
CemAkan <cemakan5014@gmail.com>
2023-02-18 15:22:11 +0300

    database connection

---
+++ connect.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+
+var NAME = process.env.NAME;
+var DB = process.env.DB;
+var DIALECT = process.env.DIALECT;
+var HOST = process.env.HOST;
+var PASSWORD = process.env.PASSWORD;
+
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize(DB, NAME, PASSWORD, {
+  host: HOST,
+  dialect: DIALECT,
+});
+
+var dataBase = {};
+
+dataBase.Todo = sequelize.import(__dirname + "/models/todo.js");
+dataBase.sequelize = sequelize;
+dataBase.Sequelize = Sequelize;
+
+module.exports = dataBase;
