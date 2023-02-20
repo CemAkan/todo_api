799a358f8f1c65d76ef0a8274576b50c2566f879
CemAkan <cemakan5014@gmail.com>
2023-02-20 15:19:37 +0300

    other requests added

--- server.js
+++ server.js
@@ -13,3 +13,23 @@ dataBase.sequelize.sync().then(() => {
     console.log("Working on port: " + PORT);
   });
 });
+
+// --> get request <--
+app.get("/todo", (req, res) => {
+  res.send("List all todo elements.");
+});
+
+// --> post request <--
+app.post("/todo", (req, res) => {
+  res.send("Adding todo elements.");
+});
+
+// --> put request <--
+app.put("/todo/:id", (req, res) => {
+  res.send("Udating todo elements.");
+});
+
+// --> delete request <--
+app.delete("/todo", (req, res) => {
+  res.send("Deleting todo elements.");
+});
